4284b7bea7c506f329217ead1c1b66439314a45f
Cobe Greene <cobegreene@gmail.com>
2019-11-03 14:36:35 -0800

    Add execute command to plugin option

--- lib/plugins/plugin-options.ts
+++ lib/plugins/plugin-options.ts
@@ -3,5 +3,6 @@ import { OptionList } from "../models";
 export abstract class PluginOptions {
     abstract submit(inputs: {[id: string]: any}): void;
     abstract options(): OptionList;
+    abstract executeCommand(cmd: string): void;
     abstract noMore(): boolean;
 }
