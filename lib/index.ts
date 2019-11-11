17c9396a4b7567fa735d00d078e534d68bd0f713
Cobe Greene <cobegreene@gmail.com>
2019-11-11 15:16:26 -0800

    Change package version

--- lib/index.ts
+++ lib/index.ts
@@ -2,5 +2,5 @@ export { Request, Response, WebService, FileService } from './services';
 export { CheckboxOption, ChoicesOption, CommandOption,
          NumberOption, Option, OptionList,
          OptionTypes, TextOption,
-        Vector } from './models';
+        FetchPluginData } from './models';
 export { FetchPlugin, PluginOptions, PluginTypes } from './plugins';
